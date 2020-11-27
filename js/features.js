1943963ed44b4c9d1c1efe0a34754d88030fdd5d
LebogangBantsijang <l.bantsijangthefirst@gmail.com>
2020-11-27 16:30:19 +0200

    commit

---
+++ js/features.js
@@ -0,0 +1,14 @@
+(function($){
+	/* Preloader */
+	$(window).on('load', function() {
+		var preloaderFadeOutTime = 500;
+		function hidePreloader() {
+			var preloader = $('.spinner-wrapper');
+			setTimeout(function() {
+				preloader.fadeOut(preloaderFadeOutTime);
+			}, 5000);
+		}
+		hidePreloader();
+	});
+
+})(jQuery);
